4cf8744ce84faa55bbd55e409746f07a9fc60d5d
juan497 <juanhernandez9731@gmail.com>
2023-07-29 01:07:46 -0700

    got rid of html route

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+//app.get("/", (req, res) => res.type('html').send(html));
 
 //app.get("/test", (req, res) => res.type('html').send(test));
 
