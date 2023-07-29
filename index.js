05c7d2f1fad8be112a7a06e1c1cc9c91e378d690
juan497 <juanhernandez9731@gmail.com>
2023-07-29 01:24:26 -0700

    added a back slash in index.html

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-//app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type('html').send(html));
 
 //app.get("/test", (req, res) => res.type('html').send(test));
 
@@ -864,7 +864,7 @@ const html = `
     </div>
 
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-    <script type="text/javascript" src="app.js"></script>
+    <script type="text/javascript" src="/app.js"></script>
 </body>
 
 </html>
